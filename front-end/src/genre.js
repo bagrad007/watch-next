604c258d2c79dfb7d8b0f10e5570d629ce746433
Luke Gill <bagrad007@gmail.com>
2021-05-03 14:55:10 -0400

    fixes relationship when rendering json to have genres associated with movies

--- front-end/src/genre.js
+++ front-end/src/genre.js
@@ -5,5 +5,21 @@ class Genre {
 function fetchGenres() {
     fetch("http://localhost:3000/genres")
         .then(resp => resp.json())
-        .then(genres => console.log(genres))
+        .then(appendGenres)
+}
+
+
+function appendGenres(genres) {
+    const genresDiv = document.getElementById("genres")
+    for (let genre of genres) {
+        const li = document.createElement("li")
+        li.innerText = genre.name
+        genresDiv.append(li)
+
+        appendMovies(genre.movies, li)
+    }
+}
+
+function postGenre() {
+
 }
--- front-end/src/movie.js
+++ front-end/src/movie.js
@@ -1,9 +1,21 @@
 class Movie {
-    constructor(name) {
-        this.name = name
-    }
 
 }
+
+
+
+function appendMovies(genre, el) {
+
+    const ul = document.createElement("ul")
+    el.append(ul)
+
+    for (let movie of genre) {
+        const movieLi = document.createElement("li")
+        movieLi.innerText = movie.name
+        ul.append(movieLi)
+    }
+}
+
 
 
 
