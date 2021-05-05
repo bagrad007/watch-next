dca811b8e094cdbf794d14929d4ef358c66fb2cf
Luke Gill <bagrad007@gmail.com>
2021-05-05 10:53:44 -0400

    adds deleteMovie function that chains off of event listener for delete button

--- front-end/src/movie.js
+++ front-end/src/movie.js
@@ -4,18 +4,27 @@ class Movie {
 
 
 
-function appendMovies(genre, el) {
+function appendMovies(movies, el) {
 
     const ul = document.createElement("ul")
     el.append(ul)
 
-    for (let movie of genre) {
+    for (let movie of movies) {
         const movieLi = document.createElement("li")
         movieLi.innerText = movie.name
         ul.append(movieLi)
+
+        const movieDelete = document.createElement("button")
+        movieDelete.className = "btn btn-danger"
+        movieDelete.innerText = "Delete"
+        movieLi.append(movieDelete)
+        movieDelete.addEventListener("click", () => deleteMovie(movie.id))
     }
 }
 
-
-
-
+function deleteMovie(movieId) {
+    fetch(`http://localhost:3000/genres/${movieId}`, {
+        method: "DELETE"
+    }).then(resp => resp.json())
+        .then(m => console.log(m))
+}
